1cc867cdfe5a07b52b1c7be03fc47312ad8537aa
Bierwart Alain <Bierwart Alain@DESKTOP-OMHDG9J>
2025-08-21 12:12:18 +0200

    input+signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';//input est une fction
 
 
 @Component({
@@ -10,15 +10,22 @@ import { Component, Input } from '@angular/core';
 })
 export class UserComponent {
 
-// cette propriete  est settable from outside
-  @Input({required: true}) avatar!: string; //!= on dit a typescript que l on est sur que la valeur sera un string mm si ca n a pas l air logique
-  @Input({required: true}) name!: string;
+// cette propriete  est settable from outside grace a input
+  //@Input({required: true}) avatar!: string; //!= on dit a typescript que l on est sur que la valeur sera un string mm si ca n a pas l air logique
+  //@Input({required: true}) name!: string;
 
-  get imagePath(){
+
+  avatar = input.required<string>();//  readonly
+  name = input.required<string>();
+
+ /* get imagePath(){
     return 'assets/users/' + this.avatar
-  }
-  onSelectUser(){
-    
+  }*/
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar()
+  })
+    onSelectUser(){
   }
 
 }
